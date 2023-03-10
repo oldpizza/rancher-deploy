3b39ce97a90ce06db9e51cfc0e27d78bb7d0fcec
first0012m <firstrock03@gmail.com>
2023-03-10 12:19:56 +0700

    edit_sign.js

--- data/signature.js
+++ data/signature.js
@@ -1,10 +1,14 @@
-jQuery(document).ready(function($){
-    
-    var canvas = document.getElementById("signature");
-    var signaturePad = new SignaturePad(canvas);
-    
-    $('#clear-signature').on('click', function(){
-        signaturePad.clear();
+document.getElementById(
+    "190a2a37-0fde-eeb5-fd3e-15c5157f4508_3f1e90f5-b594-f462-ad9a-50a95aa61b2b"
+).onclick = function () {
+    jQuery(document).ready(function ($) {
+
+        var canvas = document.getElementById("signature");
+        var signaturePad = new SignaturePad(canvas);
+
+        $('#clear-signature').on('click', function () {
+            signaturePad.clear();
+        });
+
     });
-    
-});
+};
