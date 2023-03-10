de76d5d2291b64473d09b712aa6bfb42df015864
first0012m <firstrock03@gmail.com>
2023-03-10 10:37:12 +0700

    edit_camera.js

--- data/camera.js
+++ data/camera.js
@@ -3,6 +3,10 @@ let imageName = document.getElementById("imageName")
 
 input.addEventListener("change", () => {
     let inputImage = document.querySelector("input[type=file]").files[0];
-
-    imageName.innerText = inputImage.name;
+    var reader = new FileReader();
+    reader.onloadend = function () {
+        console.log('RESULT', reader.result)
+        imageName.innerText = reader.result;
+    }
+    reader.readAsDataURL(inputImage);
 })
