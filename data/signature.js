33e2e1e4439769c65691a0f7b3b3d5fba4997a43
first0012m <firstrock03@gmail.com>
2023-03-10 12:11:31 +0700

    add_signature.js

---
+++ data/signature.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+    
+    var canvas = document.getElementById("signature");
+    var signaturePad = new SignaturePad(canvas);
+    
+    $('#clear-signature').on('click', function(){
+        signaturePad.clear();
+    });
+    
+});
