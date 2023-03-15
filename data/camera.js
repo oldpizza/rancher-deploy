04b55154e5e80c191a8b24664cf560dbef1c8c17
first0012m <firstrock03@gmail.com>
2023-03-15 11:51:33 +0700

    edit_data_camera.js

--- data/camera.js
+++ data/camera.js
@@ -1,5 +1,5 @@
 let input = document.getElementById("inputTag");
-let imageName = document.getElementById("imageName")
+let imageName = document.getElementById("f41ea8a5-fc0c-5824-b7b0-ed6229f31640_bca605c6-a29b-a237-12bf-d0a3618cc788")
 // width = screen.availWidth;
 // height = screen.availHeight;
 // popW = 640, popH = 480;
@@ -21,8 +21,8 @@ input.addEventListener("change", () => {
         //     ',top=' + top + ',left=' + left + ', scrollbars=yes');
 
         // popWindow.document.body.appendChild(proImage);
-        document.getElementById('9c5cb48c-cec4-e77e-221e-7a9c537967ec_d96771e3-3754-6b57-3a21-0a05d8bd60d9_Picture').focus();
-        document.getElementById('9c5cb48c-cec4-e77e-221e-7a9c537967ec_d96771e3-3754-6b57-3a21-0a05d8bd60d9_Picture').src = reader.result;
+        document.getElementById('f41ea8a5-fc0c-5824-b7b0-ed6229f31640_74c8f94d-26fa-8ee9-f6fd-1d017102a329_Picture').focus();
+        document.getElementById('f41ea8a5-fc0c-5824-b7b0-ed6229f31640_74c8f94d-26fa-8ee9-f6fd-1d017102a329_Picture').src = reader.result;
     }
 
     reader.readAsDataURL(inputImage);
