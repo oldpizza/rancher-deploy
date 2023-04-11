24a98ed7bc98ccfd854bba5788bcdab796488960
first0012m <firstrock03@gmail.com>
2023-04-11 17:52:28 +0700

    edi

--- smartcard/pod.js
+++ smartcard/pod.js
@@ -27,7 +27,7 @@ const uploadFunction = event => {
         var json = JSON.parse(data)
 
         document.getElementById('a793659f-9f03-b706-b925-29f73d62cf7c_f35c400f-8329-fcc9-85ef-ca73982db6dc').focus();
-        document.getElementById('a793659f-9f03-b706-b925-29f73d62cf7c_f35c400f-8329-fcc9-85ef-ca73982db6dc').innerHTML = json;
+        document.getElementById('a793659f-9f03-b706-b925-29f73d62cf7c_f35c400f-8329-fcc9-85ef-ca73982db6dc').innerHTML = data;
 
         console.log(data)
     }).catch(error => {
