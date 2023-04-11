6b125628785616ea28154cf3d5b323a85fcc6fb1
first0012m <firstrock03@gmail.com>
2023-04-11 17:03:01 +0700

    edit_

---
+++ smartcard/pod.js
@@ -0,0 +1,46 @@
+const uploadFunction = event => {
+    const files = event.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+
+    const fetchPromise = fetch('https://apipy-dev.saksiam.co.th/FilePOD', {
+        method: 'POST',
+        body: data
+    })
+
+    fetchPromise.then(response => {
+        if (response.status == 200) {
+            Swal.fire(
+                'เสร็จสิ้น',
+                'สร้างไฟล์เรียบร้อยแล้ว',
+                'success'
+            )
+            return response.json()
+        } else {
+            Swal.fire(
+                'ผิดพลาด',
+                'เกิดปัญหาบางอย่าง',
+                'error'
+            )
+        }
+    }).then(async data => {
+        var json = JSON.parse(data)
+
+        document.getElementById('a793659f-9f03-b706-b925-29f73d62cf7c_f35c400f-8329-fcc9-85ef-ca73982db6dc').focus();
+        document.getElementById('a793659f-9f03-b706-b925-29f73d62cf7c_f35c400f-8329-fcc9-85ef-ca73982db6dc').value = json;
+
+        console.log(json)
+    }).catch(error => {
+        console.error(error)
+    })
+
+    Swal.fire(
+        'กรุณารอสักครู่',
+        'กำลังสร้างไฟล์',
+        'question'
+    )
+}
+
+document.querySelector('#upload').addEventListener('change', event => {
+    uploadFunction(event)
+})
