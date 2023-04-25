785ebdceff999deeefa5718d99c273399a4124ee
first0012m <firstrock03@gmail.com>
2023-04-25 09:48:31 +0700

    edit

--- smartcard/asset.js
+++ smartcard/asset.js
@@ -24,10 +24,9 @@ const uploadFunction = event => {
             )
         }
     }).then(async data => {
-        var json = JSON.parse(data)
-        const jsons = JSON.stringify(json)
+        var json = JSON.stringify(data)
         document.getElementById('785af95c-e63a-c890-96bb-4977c188de66_7c1dc1b6-eb3e-2dbf-4a0d-e35e90a126ce_TextArea').focus();
-        document.getElementById('785af95c-e63a-c890-96bb-4977c188de66_7c1dc1b6-eb3e-2dbf-4a0d-e35e90a126ce_TextArea').value = jsons;
+        document.getElementById('785af95c-e63a-c890-96bb-4977c188de66_7c1dc1b6-eb3e-2dbf-4a0d-e35e90a126ce_TextArea').value = json;
     }).catch(error => {
         console.error(error)
     })
