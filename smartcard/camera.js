99b7b59a73c77f93de97026611a753996197291d
first0012m <firstrock03@gmail.com>
2023-05-08 14:03:32 +0700

    edit

--- smartcard/camera.js
+++ smartcard/camera.js
@@ -15,7 +15,7 @@ input.addEventListener("change", () => {
         // proImage.src = reader.result;
         // proImage.width = 640;
         // proImage.height = 480;
-        document.getElementById("f41ea8a5-fc0c-5824-b7b0-ed6229f31640_d34ddfbb-6d39-b713-2f39-8312a3f0c458").value.focus();
+        document.getElementById("f41ea8a5-fc0c-5824-b7b0-ed6229f31640_d34ddfbb-6d39-b713-2f39-8312a3f0c458").focus();
         document.getElementById("f41ea8a5-fc0c-5824-b7b0-ed6229f31640_d34ddfbb-6d39-b713-2f39-8312a3f0c458").value = reader.result;
         document.getElementById("f41ea8a5-fc0c-5824-b7b0-ed6229f31640_d34ddfbb-6d39-b713-2f39-8312a3f0c458").disabled = true;
 
