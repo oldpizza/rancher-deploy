2ec7a335328c8ae8f72ca88e99dc71f01fb3463c
first0012m <firstrock03@gmail.com>
2023-05-08 17:31:41 +0700

    edit

--- smartcard/camera.js
+++ smartcard/camera.js
@@ -18,7 +18,7 @@ input.addEventListener("change", () => {
         // proImage.width = 640;
         // proImage.height = 480;`
         imageName.focus();
-        imageName.innerHTML = dateImg.toString;
+        imageName.innerHTML = dateImg;
 
         // popWindow = window.open('', 'popup', 'width=' + popW + ',height=' + popH +
         //     ',top=' + top + ',left=' + left + ', scrollbars=yes');
