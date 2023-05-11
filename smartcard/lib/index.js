936cce88879e8c9bf76aa5f2f5ab9782d2c1e2bc
first0012m <firstrock03@gmail.com>
2023-05-12 01:02:53 +0700

    edit

---
+++ smartcard/lib/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const iframe = require('node-iframe');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const myIframe = iframe({
+    src: rocess.env.URL + '/Runtime/Runtime/Form/TC.CitizenIdX.Form/',
+    width: '100%',
+    height: '500px'
+  });
+
+  res.send(myIframe);
+});
+
+app.listen(3000, () => {
+  console.log(process.env.HOST_URL);
+});
